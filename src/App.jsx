960484aa77fc34e0e27209c51a37c509e6413737
Toufiq Islam Swoad <toufiq11swoad@gmail.com>
2024-01-24 23:54:46 +0600

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="bg-[#191D26] font-[Inter] text-white">
+        <Navbar />
+      </div>
+    </>
+  );
 }
 
 export default App;
